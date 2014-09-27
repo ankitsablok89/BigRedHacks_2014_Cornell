3cecc4eac7c0a52fbea32d73d2eeb187347bcd7c
Ankit Sablok <ankitsablok19091989@gmail.com>
2014-09-27 06:31:57 -0400

    BandsInTownDataParser.py - commit - 6

--- BandsInTownDataParser.py
+++ BandsInTownDataParser.py
@@ -1,7 +1,37 @@
 # import the following libraries to perform parsin of the BandsInTown API data
+import re
 import json
 import urllib2
+import datetime
 import dateutil.parser
+
+# this class is used to store the different attributes of a Hotel object, these are the hotels that are nearby the event
+class HotelInfo:
+
+	# this is the constructor method for the hotel objects
+	def __init__(self, pricelineJsonResponseDict):
+		self.pclnHotelID = pricelineJsonResponseDict['pclnHotelID']
+		self.longitude = pricelineJsonResponseDict['lon']
+		self.merchPrice = pricelineJsonResponseDict['merchPrice']
+		self.overallRatingScore = pricelineJsonResponseDict['overallRatingScore']
+		self.starRating = pricelineJsonResponseDict['starRating']
+		self.hotelName = pricelineJsonResponseDict['hotelName']
+		self.numGuestReviewsWithText = pricelineJsonResponseDict['numGuestReviewsWithText']
+		self.pwysRate = pricelineJsonResponseDict['pwysRate']
+		self.isRateRefundable = pricelineJsonResponseDict['isRateRefundable']
+		self.freeCancelAvail = pricelineJsonResponseDict['freeCancelAvail']
+		self.latitude = pricelineJsonResponseDict['lat']
+		self.customHMIRank = pricelineJsonResponseDict['customHMIRank']
+		self.proximity = pricelineJsonResponseDict['proximity']
+		self.originalRank = pricelineJsonResponseDict['originalRank']
+		self.remainingRooms = pricelineJsonResponseDict['remainingRooms']
+		self.cleanlinessScore = pricelineJsonResponseDict['cleanlinessScore']
+		self.diningScore = pricelineJsonResponseDict['diningScore']
+		self.locationScore = pricelineJsonResponseDict['locationScore']
+		self.staffScore = pricelineJsonResponseDict['staffScore']
+		self.guestPopularity = pricelineJsonResponseDict['guestPopularity']
+		self.tripAdvisorGuestRating = pricelineJsonResponseDict['tripAdvisorGuestRating']
+		self.tripAdvisorNumOfReviews = pricelineJsonResponseDict['tripAdvisorNumOfReviews']
 
 # this class is used to store the location information of different events where the artists will be performing in the future
 class EventLocationInfo:
@@ -62,6 +92,43 @@ if __name__ == '__main__':
 	# this is the name of the artist that the user entered
 	artistName = raw_input("Enter Artist Name : ")
 	artistInfo = ArtistInfo(artistName)
+	
+	print artistName + " is playing at the following venues -: \n"
 
 	for event in artistInfo.artistEventList:
-		event.printEventInfo()
+		print event.eventVenueName
+
+	# now we prompt the user to enter the venue where he wants to see the band playing
+	userVenue = raw_input("Enter the venue that interests you the most : ")
+
+	# this is the event that the user is most interested in visiting
+	userEvent = None
+	# check which event is the user most interested to visit out of a list of events
+	for event in artistInfo.artistEventList:
+		if event.eventVenueName == userVenue:
+			userEvent = event
+
+	# now that we have the event the user is most likely to visit we now come up with the list of best 50 hotels in the region near the hotel
+	eventHotelInfoObjects = []
+
+	for event in artistInfo.artistEventList:
+
+		if userEvent.eventVenueName == event.eventVenueName:
+
+			# these are the checkin and checkout dates of an event for the user
+			checkInDate = event.eventDateTime.date().strftime('%Y%m%d')
+			checkOutDate = (event.eventDateTime + datetime.timedelta(days=1)).date().strftime('%Y%m%d')
+
+			# get the hotel info corresponding to an event in json
+			hotelInfoJsonResponse = urllib2.urlopen('http://www.priceline.com/api/hotelretail/listing/v3/' + event.eventCityLatitude + ',' + event.eventCityLongitude + '/' + checkInDate + '/' + checkOutDate + '/' + '1' + '/' + '50?offset=0')
+			hotelInfoJsonResponseDict = json.load(hotelInfoJsonResponse)	
+
+			# iterate over all the key value pairs in the 'hotels' field's value
+			for value in hotelInfoJsonResponseDict['hotels'].values():
+				hotelInfoObject = HotelInfo(value)
+				eventHotelInfoObjects.append(hotelInfoObject)
+
+			break;
+
+	for hotels in eventHotelInfoObjects:
+		print hotels.hotelName
