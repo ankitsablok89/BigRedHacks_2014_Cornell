cc6e8d6811f4f0a0391303ad272d64d017cc773c
Ankit Sablok <ankitsablok19091989@gmail.com>
2014-09-27 17:20:29 -0400

    BandsInTownDataParser.py - commit - 9

--- BandsInTownDataParser.py
+++ BandsInTownDataParser.py
@@ -12,6 +12,105 @@ import argparse
 import dateutil.parser
 from operator import itemgetter
 
+# information needed to query the Yelp API
+API_HOST = 'api.yelp.com'
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 3
+SEARCH_PATH = '/v2/search/'
+BUSINESS_PATH = '/v2/business/'
+
+# these are OAuth credential placeholders that must be filled in by users to get data from the Yelp API
+CONSUMER_KEY = 'SMzUDUTAPROtMckDexJoWw'
+CONSUMER_SECRET = 'Pe2CmnNNEefzVt1_d-NhJCiMihY'
+TOKEN = 'P6XyNEnDrHa-azWezIQ4hGnAZVGeH9Hv'
+TOKEN_SECRET = '-zFaLfuvyyyYitPscismVh-r_mc'
+
+def request(host, path, url_params=None):
+	url_params = url_params or {}
+	url = 'http://{0}{1}?'.format(host, path)
+	consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+	oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
+
+	oauth_request.update(
+		{
+        	'oauth_nonce': oauth2.generate_nonce(),
+        	'oauth_timestamp': oauth2.generate_timestamp(),
+        	'oauth_token': TOKEN,
+        	'oauth_consumer_key': CONSUMER_KEY
+        }
+    )
+	token = oauth2.Token(TOKEN, TOKEN_SECRET)
+	oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+	signed_url = oauth_request.to_url()
+
+	print 'Querying {0} ...'.format(url)
+	conn = urllib2.urlopen(signed_url, None)
+	try:
+		response = json.loads(conn.read())
+	finally:
+		conn.close()
+	
+	return response
+
+def search(term, location):
+    """Query the Search API by a search term and location.
+
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, url_params=url_params)
+
+def get_business(business_id):
+    """Query the Business API by a business ID.
+
+    Args:
+        business_id (str): The ID of the business to query.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path)
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    response = search(term, location)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print 'No businesses for {0} in {1} found.'.format(term, location)
+        return
+
+    business_id = businesses[0]['id']
+
+    print '{0} businesses found, querying business info for the top result "{1}" ...'.format(
+        len(businesses),
+        business_id
+    )
+
+    response = get_business(business_id)
+
+    print 'Result for business "{0}" found:'.format(business_id)
+    pprint.pprint(response, indent=2)
+
 # this function is used to compute the distance between 2 latitude/longitude pairs relative to earth's radius
 def computeDistanceFromCoordinates(lat1,long1,lat2,long2):
 		
@@ -178,5 +277,13 @@ if __name__ == '__main__':
 
 	# ask the user if he wants to know more about a specific hotel or not
 	specificHotelDetails = raw_input("ENTER THE NAME OF THE HOTEL ABOUT WHICH YOU WOULD LIKE TO KNOW MORE : ")
-	
-		
+
+	# now we start working with the Yelp API
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-q', '--term', dest='term', default=DEFAULT_TERM, type=str, help='')
+	parser.add_argument('-l', '--location', dest='location', default=DEFAULT_LOCATION, type=str, help='')
+	input_values = parser.parse_args()
+	try:
+		query_api(input_values.term, input_values.location)
+	except urllib2.HTTPError as error:
+		sys.exit('Encountered HTTP error {0}. Abort program.'.format(error.code))	
