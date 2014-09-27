57eff9334d5c222cc0b01e898739aa21841a9f0a
Ankit Sablok <ankitsablok19091989@gmail.com>
2014-09-27 00:53:40 -0400

    BandsInTownDataParser.py - commit - 4

--- BandsInTownDataParser.py
+++ BandsInTownDataParser.py
@@ -25,7 +25,7 @@ class ArtistInfo:
 	def __init__(self, artistName):
 
 		# hit the BandsInTown API to get the list of attributes for the artistName passed using the get request - http://www.bandsintown.com/api/1.0/requests#artists-get 
-		artistInfoJsonResponse = urllib2.urlopen('http://api.bandsintown.com/artists/' + artistName + '.json?app_id=' + bandsInTownAppID)		
+		artistInfoJsonResponse = urllib2.urlopen('http://api.bandsintown.com/artists/' + artistName + '.json?app_id=BigRedHacks_2014')		
 		# convert the json object obtained above into a Python dictionary object
 		artistInfoJsonDictionary = json.load(artistInfoJsonResponse)
 
@@ -33,4 +33,24 @@ class ArtistInfo:
 		self.artistName = artistName
 		self.artistURLOnBandsInTown = artistInfoJsonDictionary['url']
 		self.artistMBID = artistInfoJsonDictionary['mbid']
-		self.artistUpcomingEventsCount = artistInfoJsonDictionary['upcoming_events_count']		
+		self.artistUpcomingEventsCount = artistInfoJsonDictionary['upcoming_events_count']
+
+		# now we try to get a list of all the venues where the artist is performing by hitting the BandsInTownAPI again - http://www.bandsintown.com/api/1.0/responses#events-json
+		artistEventsInfoJsonResponse = urllib2.urlopen('http://api.bandsintown.com/artists/' + self.artistName + '/events.json?app_id=BigRedHacks_2014')
+		# convert the json response obtained above into a python list which consists of event objects which in turn are dictionaries
+		artistEventsInfoJsonResponseList = json.load(artistEventsInfoJsonResponse)
+		# this is the list of EventLocationInfo objects corresponding to the artist
+		eventArtistList = []
+		for event in artistEventsInfoJsonResponseList:
+			venueDictionary = event['venue']
+			eventInfoObject = EventLocationInfo(event['url'],venueDictionary['city'],venueDictionary['name'],venueDictionary['region'], venueDictionary['country'], venueDictionary['latitude'], venueDictionary['longitude'])
+			eventArtistList.append(eventInfoObject)	
+		
+		# initialize the a list object for the artist
+		self.artistEventList = eventArtistList
+
+if __name__ == '__main__':
+	artistName = raw_input('Enter the artist name : ')
+	artist = ArtistInfo(artistName)
+	for event in artist.artistEventList:
+		print event.eventVenueName
